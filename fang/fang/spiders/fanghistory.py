15fb260445396050c16c74ed95c39575075ad822
kwvenus <kwkwvenusgod@gmail.com>
2017-10-04 09:47:02 +0800

    solve info issue

--- fang/fang/spiders/fanghistory.py
+++ fang/fang/spiders/fanghistory.py
@@ -21,14 +21,19 @@ class FangtianxiahistorySpider(scrapy.Spider):
     def parse_page2(self, response):
         general_info = {'小区':'','区域':'','学校':''}
         for item in response.css("div.trl-item2"):
-            key = item.xpath('.//div[@class="lab"]/text()').extract()[0]
-            value = item.xpath('.//div[@class="rcont"]/a/text()').extract()[0]
-            if key == u'\u5c0f\xa0\xa0\xa0\xa0\xa0\xa0\u533a':
-                general_info['小区'] = value
-            elif key == u'\u533a\xa0\xa0\xa0\xa0\xa0\xa0\u57df':
-                general_info['区域'] = value
-            elif key == u'\u5b66\xa0\xa0\xa0\xa0\xa0\xa0\u6821':
-                general_info['学校'] = value
+            key = item.xpath('.//div[@class="lab"]/text()').extract()
+            value = item.xpath('.//div[@class="rcont"]/a/text()').extract()
+            if (len(key)!=0) & (len(value)!=0):
+                if key[0] == u'\u5c0f\xa0\xa0\xa0\xa0\xa0\xa0\u533a':
+                    general_info['小区'] = value
+                elif key[0] == u'\u533a\xa0\xa0\xa0\xa0\xa0\xa0\u57df':
+                    general_info['区域'] = value
+
+
+        school = response.css("div#schoolBox>a::text").extract()
+        if len(school) != 0:
+            general_info['学校'] = school
+
 
         for item in response.xpath('//div[@class="ti-item-t"]'):
             keys = item.xpath('.//span[@class="lab"]/text()').extract()
